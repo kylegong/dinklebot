4e2415340bbaf82008932960b21a836412c606a0
Kyle Gong <kylegong@gmail.com>
2015-12-05 15:39:25 -0500

    Return hello properly as JSON

--- dinklebot.py
+++ dinklebot.py
@@ -1,14 +1,14 @@
-from flask import Flask
+import flask
 
 from data import phrases
 import slack
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def hello():
   message = phrases.get_random_phrase()
-  return slack.response(message)
+  return flask.jsonify(slack.response(message))
 
 if __name__ == '__main__':
   app.run(debug=True)
--- slack.py
+++ slack.py
@@ -1,9 +1,7 @@
-import json
-
 def response(message, private=False):
   response = {"text": message}
   if private:
     response["response_type"] = "ephemeral"
   else:
     response["response_type"] = "in_channel"
-  return json.dumps(response)
+  return response
