c9ad0b2b0cc76b31a599585878d48b98295496ad
Kyle Gong <kylegong@gmail.com>
2015-12-07 01:27:28 -0500

    Fixed bug in slack.response

--- slack.py
+++ slack.py
@@ -1,6 +1,7 @@
 def response(text, extra_args=None):
   response = {'text': text}
-  response.update(extra_args)
+  if extra_args:
+    response.update(extra_args)
   return response
 
 def get_request_text(request):
