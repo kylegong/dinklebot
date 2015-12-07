b4071316008b9fea4fd11d276ab83cd3302c4522
Kyle Gong <kylegong@gmail.com>
2015-12-07 00:07:04 -0500

    Added more tests for destiny.py

--- destiny_test.py
+++ destiny_test.py
@@ -19,7 +19,38 @@ class TestDestiny(unittest.TestCase):
       self.assertEqual('Gjallarhorn', item_data['itemName'])
 
   def test_search_item(self):
-    destiny.search_item('gjallarhorn')
+    items = destiny.search_item('gjallarhorn')
+    self.assertEqual(1, len(items))
+    item_data = items[0]
+    self.assertEqual(1274330687, item_data['itemHash'])
+    self.assertEqual('Gjallarhorn', item_data['itemName'])
+
+  def test_search_item_with_max_results(self):
+    items = destiny.search_item('suros', max_results=2)
+    self.assertEqual(2, len(items))
+
+  def test_search_item_with_category(self):
+    weapons = destiny.search_item('suros', category=destiny.WEAPON,
+                                  max_results=5)
+    self.assertLess(2, len(weapons))
+    shaders = destiny.search_item('suros', category=destiny.SHADERS,
+                                  max_results=5)
+    self.assertEqual(1, len(shaders))
+    item_data = shaders[0]
+    self.assertEqual(2561402282, item_data['itemHash'])
+    self.assertEqual('SUROS Minimalist', item_data['itemName'])
+
+  def test_format_item_data(self):
+    item_data = {
+      'itemHash': 1274330687,
+      'itemName': 'Gjallarhorn',
+      'itemTypeName': 'Rocket Launcher',
+      'tierTypeName': 'Exotic',
+    }
+    expected_text = """Gjallarhorn
+Exotic Rocket Launcher
+http://db.destinytracker.com/items/1274330687/"""
+    self.assertEqual(expected_text, destiny.format_item_data(item_data))
 
 if __name__ == '__main__':
     unittest.main()
